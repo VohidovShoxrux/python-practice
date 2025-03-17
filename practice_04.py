c10c2cc78e852c3894d8854fad9a3f44b42d7a25
VohidovShoxrux <vohidovmillatumidvori01@gmail.com>
2025-03-17 18:18:55 +0500

    python_practice_04

--- practice_04.py
+++ practice_04.py
@@ -129,3 +129,110 @@
 # print(count_leap_year([year1, year2, year3]))
 
 # 31 
+
+# def is_leap_year(year):
+#     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+# def month_days(month, year):
+#     if month == 2:
+#         if is_leap_year(year):
+#             return 29
+#         else:
+#             return 28
+#     elif month in [1,3,5,7,8,10,12]:
+#         return 31
+#     elif month in [4,6,9,11]:
+#         return 30
+#     else:
+#         return -1
+
+# def find_days(year, *months):
+#     day_list = [month_days(month, year) for month in months]
+#     return day_list
+
+# print(find_days(2020, 2, 11, 12))
+# print(find_days(2019, 2, 6, 7))
+
+# 32
+
+# def is_leap_year(year):
+#     return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+
+# def month_days(month, year):
+
+#     if month == 2:
+#         if is_leap_year(year):
+#             return 29
+#         else:
+#             return 28
+#     elif month in [1,3,5,7,8,10,12]:
+#         return 31
+#     elif month in [4,6,9,11]:
+#         return 30
+#     else:
+#         return -1 
+
+# def prev_date(day, month, year):
+
+#     if month<1 or month>12:
+#         return "Bunday oy mavjud emas"
+#     if day< 1 or day > month_days(month,year):
+#         return "Bunday snana mavjud emas"
+    
+#     if day == 1 and month == 1:
+#         day = month_days(12, year-1)
+#         month = 12
+#         year -= 1 
+    
+#     elif day == 1 and month != 1:
+#         month -= 1
+#         day = month_days(month, year)
+    
+#     else:
+#         day -= 1
+
+#     return f"{day}/{month}/{year}"
+
+# print(prev_date(29, 2,2019))
+
+# 33
+
+# def is_leap_year(year):
+#     return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+
+# def month_days(moth, year):
+#     if moth == 2:
+#         if is_leap_year(year):
+#             return 29
+#         else:
+#             return 28
+#     elif month in [1,3,5,7,8,10,12]:
+#         return 31
+#     elif month in [4,6,9,11]:
+#         return 30
+#     else:
+#         return -1
+
+# def next_date(day, month, year):
+
+#     if month<1 or month>12:
+#         return "Bunday oy mavjud emas"
+    
+#     if day < 1 or day > month_days(month, year):
+#         return "Bunday sana mavjud emas"
+
+#     if day == month_days(month, year) and month == 12:
+#         day = 1
+#         month = 1
+#         year += 1
+#     elif day == month_days(month, year) and month != 12:
+#         day = 1
+#         month += 1
+#     else:
+#         day += 1
+
+#     return f"{day}/{month}/{year}"
+
+# print(next_date(28,2,2020)) 
+
+    
