d3eddbd2d362f75d7104f3988067c739aa8699cf
VohidovShoxrux <vohidovmillatumidvori01@gmail.com>
2025-04-18 11:43:39 +0500

    python_practice_07

--- practice_07.py
+++ practice_07.py
@@ -48,3 +48,127 @@
 # text = input("Textni kiriting: ")
 
 # print(extract_str(text))
+
+# 18.04.2025
+
+# 55
+
+# def count_words(text):
+#     return len(text.split())
+
+# print(count_words(input("Textni kiriting: ")))
+
+# 56
+
+# def find_shortest_word(text):
+#     words = text.split()
+#     shortest_word = words[0]
+
+#     for word in words:
+#         if len(word) < len(shortest_word):
+#             shortest_word = word
+
+#     return f"{shortest_word} {len(shortest_word)}" 
+
+# text = input("Textni kiriting: ")
+
+# print(find_shortest_word(text))
+
+# 57 dastur
+
+# def Title(text):
+#     return text.title()
+
+# 58
+
+# def remove_extra_space(text):
+#     text = " ".join(text.split())
+#     return text
+
+# print(remove_extra_space("gar   sog'insam       seni        na      qilay"))
+
+# 59
+
+# def extract_name(text):
+#     paths = text.split("/")
+#     filepath = paths[-1]
+
+#     file = filepath.split(".")[0]
+#     return file
+
+# text = input("Faylni liriting: ")
+# print(extract_name(text))
+  
+# 60 
+# def extract_extention(text):
+#     return text.split(".")[-1]
+
+# text = input("Faylni kiriting: ")
+
+# print(extract_extention(text))
+
+# 61 
+
+# def extract_first_file_path(text):
+#     paths = text.split("/")
+#     if len(paths)>2:
+#         res = paths[1]
+#     else:
+#         res = "/"
+
+#     return res
+
+# text = input("Fileni kiriting: ")
+
+# print(extract_first_file_path(text))
+
+# 62
+
+# def extract_first_file_path(text):
+#     paths = text.split("/")
+#     if len(paths)>3:
+#         res = paths[-2]
+#     else:
+#         res = "/"
+
+#     return res
+
+# text = input("Fileni kiriting: ")
+
+# print(extract_first_file_path(text))
+
+# 63 
+
+# def check_alpha(text):
+#     letters = [char for char in text if char.islower()]
+
+#     for i in range(1, len(letters)):
+#         if letters[i] < letters[i-1]:
+#             return letters[i]
+        
+#     return 0
+
+# text = input("Text kiriting: ")
+
+# print(check_alpha(text))
+
+# 64 
+
+# def check_paran(text):
+#     balance = 0
+
+#     for index, char in enumerate(text):
+#         if char == "(":
+#             balance += 1
+#         elif char == ")":
+#             balance -= 1
+#         if balance < 0:
+#             return f"Xatolik: {index}-indeksta yopilmagan qavs."
+
+#     if balance > 0:
+#         return "Xatolik: ochilgan qavs yopilmagan."
+#     else:
+#         return "Qavslar to‘g‘ri joylashgan."
+
+# text = input("Matnni kiriting: ")
+# print(check_paran(text))
